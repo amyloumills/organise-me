adc1393e3425c851e0e3455fece07b29bb156512
Amy Miller <amyloumiller123@gmail.com>
2023-05-25 21:11:50 +0100

    switched to dialog tag

--- src/components/Modal/Modal.svelte
+++ src/components/Modal/Modal.svelte
@@ -12,9 +12,9 @@
 
 {#if showModal}
 	<div class="modal-background" on:click={closeModal} on:keydown={handleKeyDown}>
-		<div class="modal" on:click|stopPropagation on:keydown={handleKeyDown}>
+		<dialog class="modal" on:click|stopPropagation on:keydown={handleKeyDown}>
 			<slot />
-		</div>
+		</dialog>
 		<!--Stop propogation prevents bubbling-->
 	</div>
 {/if}
@@ -23,25 +23,36 @@
 
 <style>
 	.modal-background {
-		position: fixed; /* Stay in place */
-		z-index: 1; /* Sit on top */
+		position: fixed;
+		z-index: 1;
 		left: 0;
 		top: 0;
-		width: 100%; /* Full width */
-		height: 100%; /* Full height */
-		overflow: auto; /* Enable scroll if needed */
-		background-color: rgb(0, 0, 0); /* Fallback color */
-		background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+		width: 100%;
+		height: 100%;
+		overflow: auto;
+		background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.7));
+		animation: fade-in 1s;
 	}
 
 	.modal {
 		background-color: #fefefe;
-		margin: 15% auto; /* 15% from the top and centered */
+		margin: 15% auto;
 		padding: 20px;
 		border: 1px solid #888;
-		width: 40%; /* Could be more or less, depending on screen size */
+		width: 40%;
 		display: flex;
 		flex-direction: column;
 		gap: 20px;
+		transition: all 2s;
+		border-radius: 5px;
+	}
+
+	@keyframes fade-in {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
 	}
 </style>
