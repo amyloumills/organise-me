b9231a23b04bb98d7147227b1d46216b0a68c2c8
Amy Miller <amyloumiller123@gmail.com>
2023-05-25 22:45:40 +0100

    fixed types and added additional information to modal

--- src/components/Modal/EditModal.svelte
+++ src/components/Modal/EditModal.svelte
@@ -4,7 +4,7 @@
 	import Modal from './Modal.svelte';
 
 	export let showModal = false;
-	export let data: NoteData | null;
+	export let data: NoteData;
 
 	const dispatch = createEventDispatcher();
 
@@ -22,6 +22,16 @@
 	<h2>Edit Note</h2>
 	<input type="text" bind:value={data.title} />
 	<textarea bind:value={data.body} />
+	<span>
+		<input bind:checked={data.pinned} type="checkbox" id="pinned" />
+		<label for="pinned">Pin to top</label>
+	</span>
+	<label for="priority">Set Priority:</label>
+	<select bind:value={data.priority} name="priority" id="priority">
+		<option value="high">High</option>
+		<option value="normal">Normal</option>
+		<option value="low">Low</option>
+	</select>
 	<span class="button-span">
 		<button type="button" on:click={saveData}>Save</button>
 		<button type="button" on:click={closeModal}>Close</button>
